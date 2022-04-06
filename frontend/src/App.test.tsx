48df4b9bc5e41577bfe30ba0f36f5df469fd1e36
Alexey Vorobyev <alexvorobev01@gmail.com>
2022-04-06 14:20:44 +0400

    fix initial test

--- frontend/src/App.test.tsx
+++ frontend/src/App.test.tsx
@@ -1,10 +1,10 @@
-import React from 'react';
 import { render, screen } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
 
 import App from './App';
 
 test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  render(<MemoryRouter><App /></MemoryRouter>);
+  const linkElement = screen.getByText(/TimeTracker/i);
   expect(linkElement).toBeInTheDocument();
 });
